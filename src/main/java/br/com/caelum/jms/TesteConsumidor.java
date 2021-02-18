6055b71c55c16348f649740e85350c95a195c75f
Robson <robmartinhao@gmail.com>
2021-02-18 20:09:17 -0300

    Consumindo mensagens com JMS

---
+++ src/main/java/br/com/caelum/jms/TesteConsumidor.java
@@ -0,0 +1,30 @@
+package br.com.caelum.jms;
+
+import javax.jms.*;
+import javax.naming.InitialContext;
+import java.util.Scanner;
+
+public class TesteConsumidor {
+
+    public static void main(String[] args) throws Exception {
+
+        InitialContext context = new InitialContext();
+        ConnectionFactory factory = (ConnectionFactory) context.lookup("ConnectionFactory");
+
+        Connection connection = factory.createConnection();
+        connection.start();
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+        Destination fila = (Destination) context.lookup("financeiro");
+        MessageConsumer consumer = session.createConsumer(fila);
+        Message message = consumer.receive();
+
+        System.out.println("Recebendo a msg: " + message);
+
+        new Scanner(System.in).nextLine();
+
+        session.close();
+        connection.close();
+        context.close();
+    }
+}
